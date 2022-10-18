1dcd59a0ce22560212ec5199efbf41262fdc7ead
aliceakesson <alicek732@gmail.com>
2022-10-18 10:49:58 +0200

    Restart button implemented

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
-var texts = [ "Somebody once told me the world is gonna roll me. I ain't the sharpest tool in the shed. " +
- "She was looking kind of dumb with her finger and her thumb In the shape of an \"L\" on her forehead" ];
-var index = 0;
+var texts = [ "Somebody once told me" ];
+
+//  var texts = [ "Somebody once told me the world is gonna roll me. I ain't the sharpest tool in the shed. " +
+//  "She was looking kind of dumb with her finger and her thumb In the shape of an \"L\" on her forehead" ];
+
+
+ var index = 0;
 var textObject = document.getElementById('text');
 var inputObject = document.getElementById('inputObject');
+
+var startButton = document.getElementById('start');
+var startBgColor = startButton.style.backgroundColor;
+var startColor = startButton.style.color; 
+var startBgColorHover = "#378039";
+var restartBgColor = "#c23838";
+var restartColor = "#6e2020";
+var restartBgColorHover = "#4a1414";
 
 var correct = document.getElementById('correct');
 var incorrect = document.getElementById('incorrect');
 var unselected = document.getElementById('unselected');
 
 var timer = document.getElementById('timer');
-var startButton = document.getElementById('start');
-var hasStarted = false;
+var ongoingGame = false;
 
 unselected.innerHTML = texts[index];
 
-startButton.addEventListener('click', function() {
-    if(!hasStarted) {
-        startButton.style.backgroundColor = "#ddd";
-        startButton.style.color = "#555";
+const round = setInterval(timerTick, 1000);
 
-        setInterval(timerTick, 1000);
+startButton.addEventListener('click', function() {
+    if(!ongoingGame) {
+        startButton.style.backgroundColor = restartBgColor;
+        startButton.style.color = restartColor;
+        startButton.innerHTML = "RESTART"
         
-        hasStarted = true;
+        ongoingGame = true;
+    }
+    else {
+        timer.innerHTML = "00:00";
+        
+        correct.innerHTML = "";
+        incorrect.innerHTML = "";
+        unselected.innerHTML = texts[index];
+
+        inputObject.value = "";
     }
 })
+startButton.onmouseover = function() {
+    if(!ongoingGame) {
+        startButton.style.backgroundColor = startBgColorHover;
+    }
+    else {
+        startButton.style.backgroundColor = restartBgColorHover;
+    }
+}
+startButton.onmouseleave = function() {
+    if(!ongoingGame) {
+        startButton.style.backgroundColor = startBgColor;
+    }
+    else {
+        startButton.style.backgroundColor = restartBgColor;
+    }
+}
 
 inputObject.addEventListener('input', function() {
     var text = inputObject.value;
@@ -51,33 +88,44 @@ inputObject.addEventListener('input', function() {
     correct.innerHTML = texts[index].substring(0, correctIndex);
     incorrect.innerHTML = texts[index].substring(correctIndex, incorrectIndex);
     unselected.innerHTML = texts[index].substring(incorrectIndex);
+
+    if(correct.innerHTML == texts[index]) {
+        ongoingGame = false;
+        clearInterval(round);
+
+        startButton.style.backgroundColor = startBgColor;
+        startButton.style.color = startColor;
+        startButton.innerHTML = "START"
+    }
 });
 
 function timerTick() {
-    var seconds = parseInt(timer.innerHTML.substring(3));
-    var minutes = parseInt(timer.innerHTML.substring(0, 2));
+    if(ongoingGame) {
+        var seconds = parseInt(timer.innerHTML.substring(3));
+        var minutes = parseInt(timer.innerHTML.substring(0, 2));
 
-    var secondsText = "";
-    var minutesText = "";
+        var secondsText = "";
+        var minutesText = "";
 
-    seconds++;
-    if(seconds >= 60) {
-        secondsText = "00";
-        minutes++;
-    }
-    else if(seconds < 10) {
-        secondsText = "0" + seconds;
-    }
-    else {
-        secondsText = seconds;
-    }
+        seconds++;
+        if(seconds >= 60) {
+            secondsText = "00";
+            minutes++;
+        }
+        else if(seconds < 10) {
+            secondsText = "0" + seconds;
+        }
+        else {
+            secondsText = seconds;
+        }
 
-    if(minutes < 10) {
-        minutesText = "0" + minutes;
-    }
-    else {
-        minutesText = minutes;
-    }
+        if(minutes < 10) {
+            minutesText = "0" + minutes;
+        }
+        else {
+            minutesText = minutes;
+        }
 
-    timer.innerHTML = minutesText + ":" + secondsText;
+        timer.innerHTML = minutesText + ":" + secondsText;
+    }
 }
